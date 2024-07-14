ca97aa420edc07e07ecacdc7643a14fc14a2d2c4
Waffle <151867877+Wafflr450@users.noreply.github.com>
2024-07-14 10:40:46 +0200

    Add all presets

--- src/main/kotlin/birdflop-presets.kt
+++ src/main/kotlin/birdflop-presets.kt
@@ -7,6 +7,42 @@ val birdflopGradients = mapOf(
         "##084CFB",
         "#ADF3FD"
     ),
+    "SimplyMC" to arrrayOF (
+        "#084CFB",
+        "#ADF3FD"
+    ),
+    "Rainbow" to arrrayOF (
+        "#FF0000",
+        "#FF7F00",
+        "#FFFF00",
+        "#00FF00",
+        "#0000FF",
+        "#EAE4AA"
+    ),
+    "Skyline" to arrrayOF (
+        "#1488CC",
+        "#2B32B2"
+    ),
+    "Mango" to arrrayOF (
+        "#1488CC",
+        "2B32B2"
+    ),
+    "Waffle" to arrrayOF (
+        "#1488CC",
+        "#2B32B2"
+    ),
+    "Dawn" to arrrayOF (
+        "#1488CC",
+        "#2B32B2"
+    ),
+    "Rose" to arrrayOF (
+        "#1488CC",
+        "#2B32B2"
+    ),
+    "Firewatch" to arrrayOF (
+        "#1488CC",
+        "#2B32B2"
+    ),        
 )
 
 val birdflopPresets = birdflopGradients.mapValues { (_, colors) ->
